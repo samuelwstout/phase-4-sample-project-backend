5df8aa3a2023430a25a2ac31d889f9db6e919a12
samuelwstout <samuelwstout@gmail.com>
2022-08-26 12:53:12 -0500

    Able to log in Musician

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,4 +6,14 @@ class SessionsController < ApplicationController
         # retrieve a token and decode that token in order to see current musician
     end
 
+    def login
+        @musician = Musician.find_by(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], instrument: params[:instrument], location: params[:location], bio: params[:bio], media_url1: params[:media_url1], media_url2: params[:media_url2])
+        if @musician && @musician.authenticate(params[:password])
+            @token = encode_token({ musician_id: @musician.id })
+            render json: { musician: @musician, token: @token }, status: :ok
+        else
+            render json: { errors: ["Values must match"]}, status: :unprocessable_entity
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :musicians #RESTful Routes
 
   get "/get-current-musician" => "sessions#get_current_musician"
+  post "/login" => "sessions#login"
 
 end
